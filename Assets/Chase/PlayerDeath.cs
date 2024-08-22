edb22cb0cb85fde390fad88745a9da0bb5738838
Denis-Rubtsov <dr6750455@gmail.com>
2024-08-22 22:32:06 +0500

    Player's death

---
+++ Assets/Chase/PlayerDeath.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour
+{
+    [SerializeField] LayerMask _enemyMask;
+    Collider[] _enemies = new Collider[10];
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        var enemyCount = Physics.OverlapSphereNonAlloc(transform.position, 7, _enemies, _enemyMask);
+        if (enemyCount >= 6) Destroy(gameObject);
+    }
+}
